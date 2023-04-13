3eb567bdf20497284b13f0da4680cd061f66c968
Scott <scott.ti312@gmail.com>
2023-04-13 16:33:00 -0700

    Add create author controller

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,6 @@
 const Author = require('../models/author');
 const Book = require('../models/book');
+const { body, validationResult } = require("express-validator");
 
 exports.author_list = async (req, res) => {
   try {
@@ -33,14 +34,74 @@ exports.author_detail = async (req, res) => {
 };
 
 // Display Author create form on GET.
-exports.author_create_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+exports.author_create_get = (req, res, next) => {
+  res.render("author_form", { title: "Create Author" });
 };
 
 // Handle Author create on POST.
-exports.author_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author create POST");
-};
+exports.author_create_post = [
+  body("first_name")
+  .trim()
+  .isLength({ min: 1 })
+  .escape()
+  .withMessage("First name must be specified.")
+  .isAlphanumeric()
+  .withMessage("First name has non-alphanumeric characters."),
+
+  body("family_name")
+  .trim()
+  .isLength({ min: 1 })
+  .escape()
+  .withMessage("Family name must be specified.")
+  .isAlphanumeric()
+  .withMessage("Family name has non-alphanumeric characters"),
+
+  body("date_of_birth", "Invalid date of birth")
+  .optional({ checkFalsy: true })
+  .isISO8601()
+  .toDate(),
+
+  body("date_of_death", "Invalid date of death")
+  .optional({ checkFalsy: true })
+  .isISO8601()
+  .toDate(),
+
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("author_form", {
+        title: "Create Author",
+        author: req.body,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      try {
+        const found_author = await Author.findOne({ 
+          $and: [
+            { first_name: req.body.first_name },
+            { last_name: req.body.last_name }
+          ]
+         }).exec();
+          console.log(found_author);
+        if (found_author) {
+          res.redirect(found_author.url)
+        } else {
+          const author = new Author({
+            first_name: req.body.first_name,
+            family_name: req.body.family_name,
+            date_of_birth: req.body.date_of_birth,
+            date_of_death: req.body.date_of_death,
+          });
+          await author.save();
+          res.redirect(author.url);
+        }
+      } catch (err) {
+        return next(err);
+      }
+    }
+  }
+]
 
 // Display Author delete form on GET.
 exports.author_delete_get = (req, res) => {
